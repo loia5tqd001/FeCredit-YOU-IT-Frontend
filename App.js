a86c2ee64b2544314b94f3546cf1fc14651d21d3
Loi <loia5tqd001@gmail.com>
2019-10-29 18:52:43 +0700

    init project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import _App from './src'
+
+export default function App() {
+  return <_App />
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
